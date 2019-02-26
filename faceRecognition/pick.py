e7de141681e05f561a72616d1dd3c20b1afdfe98
youchangxin <423884489@qq.com>
2019-02-26 13:46:29 +0800

    将文件名改为时间戳

--- faceRecognition/pick.py
+++ faceRecognition/pick.py
@@ -1,13 +1,30 @@
 import cv2
-#训练识别   人脸
+import datetime
+
+# 训练识别人脸
+out_path = r'F:\GraduationProject\pictures\myface'
+
 cap = cv2.VideoCapture(0)
-while(1):
-    # get a frame
-    ret, frame = cap.read()
-    # show a frame
-    cv2.imshow("capture", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        cv2.imwrite("fangjian2.jpeg", frame)
+faceIndex = 0
+while True:
+    if faceIndex < 10000:
+        if faceIndex % 100 ==0 :
+            print('已截图 %d 张' %faceIndex)
+
+        # get a frame
+        ret, img = cap.read()
+        # 需要先转换为灰度图
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        cv2.imshow("capture", gray_img)  # 显示画面
+
+        faceIndex += 1
+        nowTime = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')  # 获取当前时间戳
+        cv2.imwrite(out_path +'\\' + str(nowTime) + str(faceIndex) + ".jpg", gray_img)    # 保存图片
+        # 延迟一毫秒 将字符转化
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cap.release()             # 释放对象
+            cv2.destroyAllWindows()   # 销毁窗口
+            break
+    else:
+        print('完成截图')
         break
-cap.release()
-cv2.destroyAllWindows()
